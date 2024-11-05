492758ccb4965fb6144a3d42186a7ed2f8f666f1
Nethravathibs123 <nethranethra451@gmail.com>
2024-11-05 18:36:58 +0530

    first commit

--- public/index.js
+++ public/index.js
@@ -5,7 +5,7 @@ const errorMsg = document.getElementById('error');
 
 let users = [];
 
-const port = 3650;
+
 
 signUpForm.addEventListener('submit', async(event) => {
     event.preventDefault();
@@ -14,7 +14,7 @@ signUpForm.addEventListener('submit', async(event) => {
     const password = document.getElementById('password').value;
   
     try{
-        const response = await axios.post(`http://localhost:${port}/user/signup`,{username, email, password});
+        const response = await axios.post(`http://localhost:3000/user/signup`,{username, email, password});
         users.push(response.data);
 
         document.getElementById('username').value = "";
@@ -23,14 +23,16 @@ signUpForm.addEventListener('submit', async(event) => {
 
         errorMsg.textContent = '';
     }
-    catch(error){
-        if(error.response && (error.response.status === 409 || error.response.status === 404)) {
-            document.getElementById('email').value = "";
-            document.getElementById('password').value = "";
+    catch(error) {
+        document.getElementById('email').value = "";
+        document.getElementById('password').value = "";
+        
+        if (error.response && error.response.data && error.response.data.message) {
             errorMsg.textContent = `Error: ${error.response.data.message}`;
         } else {
-            console.log('Error adding user: ',error);
-            errorMsg.textContent = '';
+            console.log('Error adding user:', error);
+            errorMsg.textContent = 'An unexpected error occurred.';
         }
     }
+    
 });
